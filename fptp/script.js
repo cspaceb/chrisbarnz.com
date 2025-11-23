42a3b9dd43dfb350949baf00747cdba252b546bb
Chris <14049168+cspaceb@users.noreply.github.com>
2025-11-23 11:39:15 -0600

    Delete fptp directory

--- fptp/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggle = document.querySelector('.menu-toggle');
-  const menu = document.querySelector('.menu');
-
-  toggle.addEventListener('click', () => {
-    menu.classList.toggle('open');
-  });
-});
